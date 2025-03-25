9156359d31dc4d9fcba6014d6a9cdd2616d07004
KalineDiaas <giselia.dias@alunos.ufersa.edu.br>
2025-03-25 19:20:37 -0300

    Add: feature Simbolos

--- src/Components/PasswordGenerator.jsx
+++ src/Components/PasswordGenerator.jsx
@@ -3,10 +3,12 @@ import './PasswordGenerator.css';
 import { ToastContainer } from 'react-toastify';
 
 const lowercaseList = 'abcdefghijklmnopqrstuvwxyz';
+const symbolsList = "!@#$%^&*()_+-=[]{}|;:,.<>?";
 
 function PasswordGenerator() {
 
     const [password, setPassword] = useState('');
+    const [includeSymbols, setIncludeSymbols] = useState(false);
     const passwordLength = 10; // Tamanho fixo da senha
 
     useEffect(() => {
@@ -14,12 +16,14 @@ function PasswordGenerator() {
     },[]);
 
     const generatePassword = () => {
-        const characterList = lowercaseList;
+        let characterList = lowercaseList;
+        if (includeSymbols) characterList += symbolsList;
+        
         let tempPassword = '';
         const characterListLength = characterList.length;
 
         for (let i = 0; i < passwordLength; i++) {
-            const characterIndex = Math.round(Math.random() * characterListLength);
+            const characterIndex = Math.round(Math.random() * (characterListLength - 1));
             tempPassword += characterList.charAt(characterIndex);
         }
 
@@ -45,6 +49,16 @@ function PasswordGenerator() {
                                 <div className="checkbox-field">
                                     <input type="checkbox" name="lower" id="lower" checked disabled />
                                     <label htmlFor="lower">Incluir Minúsculas (a-z)</label>
+                                </div>
+                                <div className="checkbox-field">
+                                    <input 
+                                        type="checkbox" 
+                                        name="symbols" 
+                                        id="symbols" 
+                                        checked={includeSymbols}
+                                        onChange={(e) => setIncludeSymbols(e.target.checked)}
+                                    />
+                                    <label htmlFor="symbols">Incluir Símbolos (&-#)</label>
                                 </div>
                             </div>
                         </div>
